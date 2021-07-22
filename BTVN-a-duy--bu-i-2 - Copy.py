516cbdc2c2e957564b2f8110c482339f8fbdcd82
ducphucphung <87252259+ducphucphung@users.noreply.github.com>
2021-07-22 11:24:15 +0700

    Add files via upload

--- BTVN-a-duy--bu-i-2 - Copy.py
+++ BTVN-a-duy--bu-i-2 - Copy.py
@@ -12,6 +12,19 @@ def giaithua(n):
         return a
 giaithua(n)
 print(giaithua(n))
+#Bài 3:
+a=str(input("Nhập một chuỗi"))
+b=0
+c=0
+for i in a:
+    if i.isupper():
+        b=b+1
+    if i.islower():
+        c=c+1
+print(b)
+print(c)
+
+       
 #Bài 4:
 list1=[1,2,3,4,5,6,7,8,9]
 c=0
@@ -20,21 +33,28 @@ for i in  list1:
 print(c)
 #Bài 5:
 list_a=["hôm nay", "là", "thứ ba"]
-b=input("Nhập một cái gì đó tùy thích")
+b=input("Nhập một số tùy thích")
 for i in list_a:
     if len(i)>len(b):
         print(i)
     else:
         continue
 #Bài 6:
-a=list(input("Nhập một chuỗi j đó").split())
-def cau_moi(a):
-    chuỗi=''
-    for từ in a:
-        if từ not in chuỗi:   
-           chuỗi = chuỗi +' '+ từ
-    return chuỗi
-b=list(cau_moi(a).split())
-print(b)
-
+a=list(input("Nhập một list j đó").split())
+b=str(a)
+for i in b:
+    if b.count(i)>1:
+        c=set(b)
+        d=c.remove(i)
+        print(d)
+    else:
+        break
+        
+#Problem cuối:
+a=str(input("Nhập một số tự nhiên bất kì"))
+b=str(input("Nhập một số tự nhiên bất kì"))
+if a[0] and b[0] ==0:
+    skip
+else:
+   print(str(a+b))
 
